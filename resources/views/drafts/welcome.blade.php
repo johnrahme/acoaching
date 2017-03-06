647ad37992600227083c036f7f10140748837f85
John Rahme <john.rahme.se@gmail.com>
2017-03-06 15:37:25 +0100

    Todo uppdaterad och annat?

--- resources/views/drafts/welcome.blade.php
+++ resources/views/drafts/welcome.blade.php
@@ -1,22 +1,26 @@
 <style>
 
-    h2{
+    h2 {
         color: #467569;
         text-align: center;
     }
 </style>
 
-<div class = "row">
-    <div class = "col-md-2">
-
+<div class="row">
+    <div class="col-md-12"><h4 style="text-align: left;"><i><font face="Helvetica">Har du kommit till en punkt i livet
+                    när det måste ske en förändring på ett eller flera plan?</font></i></h4><h4
+                style="text-align: left;"><i><font face="Helvetica">Eller bara veta i vilken riktning du ska
+                    gå?&nbsp;<br></font></i></h4><h4 style="text-align: left;"><i><font face="Helvetica">Vad vill du
+                    göra med ditt liv?</font></i></h4><h4 align="center" style="text-align: left; "><br></h4><h4
+                align="center" style="text-align: left; ">Jag vill inspirera till att må bättre genom att ändra tänket,
+            bli modig i ditt liv, uppleva naturen, motionera mera och därmed få harmoni i kropp och själ! Det finns
+            ingen mall
+            för hur livet skall levas. Stanna upp o fundera över hur du vill leva
+            det? Reflektera över vad som ger dig glädje, lust och mening? Vilket
+            innehåll vill du ge ditt liv?<br></h4><h4 style="text-align: left;"><font face="Helvetica"><span
+                        style="color: inherit;">A coaching erbjuder professionell coaching med stort A, med dig i fokus. Hitta dig själv, utveckla dina mål&nbsp;</span><span
+                        style="color: inherit;">och ta tag i och förändra ditt liv.</span></font></h4>
+        <div align="center"><strong>
+            </strong></div>
     </div>
-    <div class = "col-md-8">
-        <p>
-            A coaching erbjuder professionell coaching med stort A, med Dig i fokus. Hitta dig själv och utveckla dina mål som tar tag och förändrar ditt liv.
-           </p>
-    </div>
-    <div class = "col-md-2">
-
-    </div>
-
 </div>
--- resources/views/layouts/default_home.blade.php
+++ resources/views/layouts/default_home.blade.php
@@ -15,7 +15,7 @@
 @yield('styles')
 <!-- Scripts -->
     <script>
-        window.Laravel = <?php echo json_encode([
+        window.Laravel =; <?php echo json_encode([
                 'csrfToken' => csrf_token(),
         ]); ?>
     </script>
